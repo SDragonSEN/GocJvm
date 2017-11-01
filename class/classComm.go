2f1fc8367b40e5c347729c53d4affa998d4d5472
SDragonSEN <SDragonSEN@163.com>
2017-11-01 23:35:44 +0800

    第三次提交(2017/11/01 23:31)：新增UserClassPath的解析。 因为陪某人刷题的原因，最近都没怎么写Jvm的代码，下一步把启动类路径和拓展类路径的加载搞定，就可以开始激动人心的解析.class文件了

--- class/classComm.go
+++ class/classComm.go
@@ -1,5 +1,36 @@
 package class
 
+import (
+	"strings"
+)
+
 type Class interface {
 	ReadClass(classname string) ([]byte, error)
+}
+
+var BootstrapClassPath []Class //启动类路径
+var ExtensionClassPath []Class //拓展类路径
+var UserClassPath []Class      //用户类路径
+
+func InitClassPath(userPath string) {
+	paths := strings.Split(userPath, ";")
+	for _, path := range paths {
+		if strings.HasSuffix(path, ".jar") || strings.HasSuffix(path, ".zip") {
+			jar, err := NewJarEntry(path)
+			if err != nil {
+				UserClassPath = append(UserClassPath, jar)
+			}
+		} else if strings.HasSuffix(path, "/*") {
+			dir, err := NewDirEntry(path, true)
+			if err != nil {
+				UserClassPath = append(UserClassPath, dir)
+
+			}
+		} else {
+			dir, err := NewDirEntry(path, false)
+			if err != nil {
+				UserClassPath = append(UserClassPath, dir)
+			}
+		}
+	}
 }
--- main/main.go
+++ main/main.go
@@ -2,10 +2,11 @@
 package main
 
 import (
+	"../class"
 	"../startup"
 )
 
 func main() {
-
 	startup.ParseCmd()
+	class.InitClassPath(startup.CmdPara.Jar)
 }
