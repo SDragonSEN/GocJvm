8fabe433e36df128234737d85a437c04c8b8d00d
SDragonSEN <SDragonSEN@163.com>
2017-12-25 23:16:24 +0800

    第四次提交(2017/12/25 23:06):1、新增启动类拓和展类路径加载，其实早就写好，一直没提交;2、新增内存管理的接口定义，这块之前想用C来弄，考虑了一下，其实用Go来模拟也是一样的，本身目的也只是练习。其次，最近一直都没动手写GoJVM，一是花了些时间去学习练习算法，二是去学习了一部分虚拟机的具体知识，包括.class文件的格式，字节码指令的定义，三是考虑怎么去设计，最初想法是将内存的设计和垃圾回收算法依赖在一起设计，但这样好像弄得太复杂，不一定掌控的住，所以现在还是分开设计，牺牲效率，保证解耦。内存这块的设计目前还是打算按照操作系统课程里学习的算法为主要枝干，结合虚拟机的实际使用进行优化。

--- class/classComm.go
+++ class/classComm.go
@@ -1,6 +1,7 @@
 package class
 
 import (
+	"os"
 	"strings"
 )
 
@@ -13,24 +14,43 @@ var ExtensionClassPath []Class //拓展类路径
 var UserClassPath []Class      //用户类路径
 
 func InitClassPath(userPath string) {
+	initUserClassPath(userPath)
+}
+
+//指定用户Class路径,即-cp后的路径
+func initUserClassPath(userPath string) {
 	paths := strings.Split(userPath, ";")
 	for _, path := range paths {
 		if strings.HasSuffix(path, ".jar") || strings.HasSuffix(path, ".zip") {
 			jar, err := NewJarEntry(path)
-			if err != nil {
+			if err == nil {
 				UserClassPath = append(UserClassPath, jar)
 			}
 		} else if strings.HasSuffix(path, "/*") {
 			dir, err := NewDirEntry(path, true)
-			if err != nil {
+			if err == nil {
 				UserClassPath = append(UserClassPath, dir)
 
 			}
 		} else {
 			dir, err := NewDirEntry(path, false)
-			if err != nil {
+			if err == nil {
 				UserClassPath = append(UserClassPath, dir)
 			}
 		}
+	}
+}
+
+//初始化启动类和拓展类路径
+func initBootstrapClassPath() {
+	classpath := os.Getenv("classpath")
+	if classpath != nil {
+		dir, err := NewDirEntry(classpath, false)
+		if err == nil {
+			BootstrapClassPath = append(BootstrapClassPath, dir)
+		}
+		dir, err = NewDirEntry(classpath+"/lib/ext", false)
+
+		ExtensionClassPath = append(ExtensionClassPath, dir)
 	}
 }
